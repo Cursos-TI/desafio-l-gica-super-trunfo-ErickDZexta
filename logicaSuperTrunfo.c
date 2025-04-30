b104b436710a6edd926b3304f4568aa9f23c57be
ErickDZexta <ericklusca23@gmail.com>
2025-04-30 13:49:03 +0000

    Supertrunfoparte2

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,18 +8,71 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    char estado1[20] = "Minas Gerais", codigo1[4] = "A01";
+    int populacao1, numeroDePontos1;
+    float pib1, area1;
 
-    
+    char estado2[20] = "Paraná", codigo2[4] = "A02";
+    int populacao2, numeroDePontos2;
+    float pib2, area2;
+
+
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
 
+    
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
-    // 
     // (Repita para cada propriedade)
+
+
+    // Carta 1
+
+    printf("== Dados da Carta 1 ==\n");
+    printf("%s\n", estado1);
+    printf("%s\n", codigo1);
+
+    printf("Insira a população de Minas Gerais:\n");
+    scanf("%d", &populacao1);
+
+   printf("Insira o número de pontos turísticos de Minas Gerais: \n");
+   scanf("%d", &numeroDePontos1);
+
+   printf("Insira o pib per capita de MG:\n");
+   scanf("%f", &pib1);
+
+   printf("Insira a Area por KM de MG:\n");
+   scanf("%f", &area1);
+
+   // Carta 2
+
+   printf("== Dados da Carta 2 ==\n");
+    printf("%s\n", estado2);
+    printf("%s\n", codigo2);
+
+    printf("Insira a população de Paraná:\n");
+    scanf("%d", &populacao2);
+
+   printf("Insira o número de pontos turísticos de Paraná: \n");
+   scanf("%d", &numeroDePontos2);
+
+   printf("Insira o pib per capita de PR:\n");
+   scanf("%f", &pib2);
+
+   printf("Insira a Area por KM de PR:\n");
+   scanf("%f", &area2);
+
+   if (populacao1 > populacao2){
+        printf("Carta 1 ganhou da Carta 2 em População!");
+   }else {
+        printf("Carta 2 ganhou da Carta 1 em população!");
+   }
+
+    
+
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
